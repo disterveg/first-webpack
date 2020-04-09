8123a98c53697259e8281e2a15e67970723a719e
radrigo <cherkashin.da@gazprom-neft.ru>
2020-04-09 17:37:48 +0300

    add lib and add script mode watch

--- src/modules/header.component.js
+++ src/modules/header.component.js
@@ -1,1 +1,9 @@
-console.log('header component');
+import $ from 'jquery';
+
+$('<h1 />')
+    .text('hello world jquery')
+    .css({
+        textAlign: 'center',
+        color: 'red'
+    })
+    .appendTo($('header'));
